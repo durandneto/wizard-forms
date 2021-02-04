ccaa4e2b506051aeb981dfbdafffd30c2710b29a
Durand Neto <durandneto@Durands-MacBook-Air.local>
2021-02-04 17:15:29 -0500

    adding progrees label on top bar

--- src/components/ProgressHeader.tsx
+++ src/components/ProgressHeader.tsx
@@ -3,6 +3,9 @@ import { Progress } from "reactstrap"
 const ProgressHeader = () => (
     <div className="progress_container_top">
         <div className="progress_container_top_left">
+            <p>Progress</p>
+        </div>
+        <div className="progress_container_top_middle">
             <Progress value={90}>10/21</Progress>
         </div>
         <div className="progress_container_top_right">
